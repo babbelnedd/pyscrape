e1ab68e5ba29297c5df547e1e28b87bf13c9db08
Lucas Schad <schad.lucas@gmail.com>
2014-01-05 11:40:52 +0100

    Improved RegEx Folder names can now include *any* values within brackets ( ) without breaking the match

--- pyscrape/RegEx.py
+++ pyscrape/RegEx.py
@@ -1,30 +1,50 @@
 def get_movie(title):
-    import re
+    import re, os
+
+    def remove_brackets(title):
+        from pyscrape import utils
+
+        f = os.path.join(utils.get_root(), 'system', 'replace')
+        chars = []
+        for char in open(f).readlines():
+            char = char.split(':')[0].strip()
+            if char != '':
+                chars.append(char)
+
+        regex = '\([a-z A-Z 0-9 <>\\|\\\._\-/'
+        for char in chars:
+            regex += char
+        regex += ']*\)'
+
+        for match in re.findall(regex, title):
+            title = title.replace(match,'')
+        return title
+
+    def remove_double_spaces(string):
+        while '  ' in string:
+            string = string.replace('  ', ' ')
+        return string
 
     def get_year(title):
         rx = re.search('\([0-9+]{4}\)', title)
         year = ''
         if rx:
             year = rx.group()
-        return year
+        return year.replace('(', '').replace(')', '')
 
     def get_imdb_id(title):
         rx = re.search('\(tt[0-9]{7}\)', title)
         imdb_id = ''
         if rx:
             imdb_id = rx.group()
-        return imdb_id
+        return imdb_id.replace('(', '').replace(')', '')
 
 
     year = get_year(title)
     imdb_id = get_imdb_id(title)
 
-    _title = title.replace(year, '').replace(imdb_id, '').strip()
-    while '  ' in _title:
-        _title = _title.replace('  ', ' ')
-
-    year = year.replace('(', '').replace(')', '')
-    imdb_id = imdb_id.replace('(', '').replace(')', '')
+    _title = remove_brackets(title).strip()
+    _title = remove_double_spaces(_title)
 
     result = {'title': _title, 'year': year, 'imdbID': imdb_id}
     return result
