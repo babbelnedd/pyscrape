fcd2ab3d66f087afb327268587e1e57abc2369bc
SchadLucas <schad.lucas@gmail.com>
2014-02-06 19:33:35 +0100

    Implemented Cached decorator

--- src/Decorator.py
+++ src/Decorator.py
@@ -1,1 +1,17 @@
-__author__ = 'lucas'
+from Logger import log, LogLevel
+
+
+class Cached(object):
+    def __init__(self, function):
+        self.func = function
+        self.cache = {}
+
+    def __call__(self, *args):
+        args = args[0]
+
+        if args in self.cache:
+            log('Found Cached Result', LogLevel.Debug)
+        else:
+            self.cache[args] = self.func(args)
+
+        return self.cache[args]
