4ec1d219dbaa62e18ba47187c9090afaa07f0d37
SchadLucas <schad.lucas@gmail.com>
2014-02-09 01:10:53 +0100

    Implemented unit tests for existing tests

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,4 +1,24 @@
+# -*- coding: utf-8 -*-
+import unittest
 from src import utils
 
-utils.ping('127.0.0.1', '80')
-assert utils.ping('127.0.0.1', 9728) == False
+
+class UtilsTests(unittest.TestCase):
+    def test_ping(self):
+        self.assertIs(utils.ping('127.0.0.1', 9728), False, "ping() failed")
+
+    def test_replace(self):
+        self.assertEqual(utils.replace('ä'), 'ae')
+        self.assertEqual(utils.replace('ö'), 'oe')
+        self.assertEqual(utils.replace('ü'), 'ue')
+        # ...
+
+    def test_get_movie_extensions(self):
+        extensions = utils.get_movie_extensions()
+        self.assertIn('.mkv', extensions)
+        self.assertIn('.avi', extensions)
+        self.assertIn('.mpg', extensions)
+        # ...
+
+if __name__ == '__main__':
+    unittest.main()
