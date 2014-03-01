d50e8b5d33480ba77d77914465e839d8f89837dd
Lucas Schad <schad.lucas@gmail.com>
2014-03-01 16:11:41 +0100

    Edited PluginLoader
    Belongs to #15

--- src/PluginLoader.py
+++ src/PluginLoader.py
@@ -1,63 +1,34 @@
 import imp
 import os
 
-import src.utils
+import src.plugins
 
 
-__root = src.utils.get_root()
-_plugin_root = os.path.join(__root, 'plugins')
-_main_module = '__init__'
+plugin_root = src.plugins.__path__[0]
+main_module = '__init__'
 
 
-def _get_plugins():
-    """
-    Get all custom-plugins from the plugin root
-    """
+def _search_plugins():
     plugins = []
-    for plugin_name in os.listdir(_plugin_root):
-        location = os.path.join(_plugin_root, plugin_name)
-        if not os.path.isdir(location) or not _main_module + '.py' in os.listdir(location):
+
+    for location in [os.path.join(plugin_root, path) for path in os.listdir(plugin_root)
+                     if os.path.isdir(os.path.join(plugin_root, path))]:
+        if not main_module + '.py' in os.listdir(location):
             continue
-
-        module = imp.find_module(_main_module, [location])
-        plugin_info = imp.load_module(_main_module, *module)
-        info = imp.find_module(plugin_info.__plugin__, [location])
-
-        plugins.append({'name': plugin_name, 'plugin': info, 'info': plugin_info})
+        info = imp.find_module(main_module, [location])
+        plugins.append({'name': location, 'info': info})
     return plugins
 
 
 def _load_plugin(plugin):
-    """
-    Load and returns a plugin.
-
-    @param  plugin      The plugin that shall be loaded.
-                        Expects a result of imp.find_module()
-    """
-    return imp.load_module(_main_module, *plugin)
+    return imp.load_module(main_module, *plugin['info'])
 
 
 def get_plugins(plugin_type=None):
-    """
-    Load all custom-plugins.
-
-    @param  plugin_type     Load only plugins from given type. Optional.
-    """
-    from Logger import LogLevel, log
-
     plugins = []
-
-    for p in _get_plugins():
-        log('Loading plugin \'' + p['info'].__name__ + '\' v' + p['info'].__version__, LogLevel.Debug)
-        try:
-            obj = _load_plugin(p['plugin'])
-            ptype = p['info'].__type__
-            plugin = {'obj': obj, 'type': ptype}
-
-            if plugin_type is None or (plugin_type is not None and plugin_type == ptype):
-                plugins.append(plugin)
-        except Exception as exception:
-            log('Failed to import plugin', LogLevel.Debug)
-            log(exception.message, LogLevel.Debug)
+    for plugin in _search_plugins():
+        plugin = _load_plugin(plugin).load()
+        if plugin_type is None or plugin.info['type'] == plugin_type:
+            plugins.append(plugin)
 
     return plugins
