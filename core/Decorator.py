6c0c44c197e178f8a64615261c75821535be4640
Lucas Schad <schad.lucas@gmail.com>
2014-03-01 20:25:58 +0100

    Fixed log path in Decorator

--- core/Decorator.py
+++ core/Decorator.py
@@ -40,7 +40,7 @@ def split_log():
     import core
 
     root = os.path.abspath(os.path.join(core.__path__[0], os.pardir))
-    path = os.path.join(root, 'logs')
+    path = os.path.join(root, '.logs')
     logfile = os.path.join(path, 'pyscrape.log')
     errorfile = os.path.join(path, 'error.log')
     kilobyte = 1024
