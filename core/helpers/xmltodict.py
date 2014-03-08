b157d778ef8ccbd6d6d532c8014c2f0905dd6336
Lucas Schad <schad.lucas@gmail.com>
2014-03-08 19:00:19 +0100

    Implemented methods to parse xml to a dictionary
    belongs to #18
    
    methods
     - xml to dict
     - dict to xml
    From python recipe http://code.activestate.com/recipes/573463-converting-xml-to-dictionary-and-back/

---
+++ core/helpers/xmltodict.py
@@ -0,0 +1,142 @@
+from xml.etree import ElementTree
+
+
+class XmlDictObject(dict):
+    """
+    Adds object like functionality to the standard dictionary.
+    """
+
+    def __init__(self, initdict=None):
+        if initdict is None:
+            initdict = {}
+        dict.__init__(self, initdict)
+
+    def __getattr__(self, item):
+        return self.__getitem__(item)
+
+    def __setattr__(self, item, value):
+        self.__setitem__(item, value)
+
+    def __str__(self):
+        if self.has_key('_text'):
+            return self.__getitem__('_text')
+        else:
+            return ''
+
+    @staticmethod
+    def Wrap(x):
+        """
+        Static method to wrap a dictionary recursively as an XmlDictObject
+        """
+
+        if isinstance(x, dict):
+            return XmlDictObject((k, XmlDictObject.Wrap(v)) for (k, v) in x.iteritems())
+        elif isinstance(x, list):
+            return [XmlDictObject.Wrap(v) for v in x]
+        else:
+            return x
+
+    @staticmethod
+    def _UnWrap(x):
+        if isinstance(x, dict):
+            return dict((k, XmlDictObject._UnWrap(v)) for (k, v) in x.iteritems())
+        elif isinstance(x, list):
+            return [XmlDictObject._UnWrap(v) for v in x]
+        else:
+            return x
+
+    def UnWrap(self):
+        """
+        Recursively converts an XmlDictObject to a standard dictionary and returns the result.
+        """
+
+        return XmlDictObject._UnWrap(self)
+
+
+#region Private Methods
+
+
+def _convert_dict_to_xml_recursive(parent, dictitem):
+    assert type(dictitem) is not type([])
+
+    if isinstance(dictitem, dict):
+        for (tag, child) in dictitem.iteritems():
+            if str(tag) == '_text':
+                parent.text = str(child)
+            elif type(child) is type([]):
+                # iterate through the array and convert
+                for listchild in child:
+                    elem = ElementTree.Element(tag)
+                    parent.append(elem)
+                    _convert_dict_to_xml_recursive(elem, listchild)
+            else:
+                elem = ElementTree.Element(tag)
+                parent.append(elem)
+                _convert_dict_to_xml_recursive(elem, child)
+    else:
+        parent.text = str(dictitem)
+
+
+def _convert_xml_to_dict_recursive(node, dictclass):
+    nodedict = dictclass()
+
+    if len(node.items()) > 0:
+        # if we have attributes, set them
+        nodedict.update(dict(node.items()))
+
+    for child in node:
+        # recursively add the element's children
+        newitem = _convert_xml_to_dict_recursive(child, dictclass)
+        if nodedict.has_key(child.tag):
+            # found duplicate tag, force a list
+            if type(nodedict[child.tag]) is type([]):
+                # append to existing list
+                nodedict[child.tag].append(newitem)
+            else:
+                # convert to list
+                nodedict[child.tag] = [nodedict[child.tag], newitem]
+        else:
+            # only one, directly set the dictionary
+            nodedict[child.tag] = newitem
+
+    if node.text is None:
+        text = ''
+    else:
+        text = node.text.strip()
+
+    if len(nodedict) > 0:
+        # if we have a dictionary add the text as a dictionary value (if there is any)
+        if len(text) > 0:
+            nodedict['_text'] = text
+    else:
+        # if we don't have child nodes or attributes, just set the text
+        nodedict = text
+
+    return nodedict
+
+
+#endregion
+
+
+def dict_to_xml(xml_dict):
+    """
+    Converts a dictionary to an XML ElementTree Element
+    """
+
+    root_tag = xml_dict.keys()[0]
+    root_tag = ElementTree.Element(root_tag)
+    _convert_dict_to_xml_recursive(root_tag, xml_dict[root_tag])
+    return root_tag
+
+
+def xml_to_dict(root, xml_dict=XmlDictObject):
+    """
+    Converts an XML file or ElementTree Element to a dictionary
+    """
+
+    if isinstance(root, str):
+        root = ElementTree.parse(root).getroot()
+    elif not isinstance(root, ElementTree.Element):
+        raise TypeError('Expected ElementTree.Element or file path string')
+
+    return xml_dict({root.tag: _convert_xml_to_dict_recursive(root, xml_dict)})
