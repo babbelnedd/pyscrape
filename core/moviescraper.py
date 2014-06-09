ce44564ecba89f02000509e38345d4603ac4e671
Lucas Schad <schad.lucas@gmail.com>
2014-06-09 03:19:16 +0200

    Fixed project imports for Scrapers

--- core/moviescraper.py
+++ core/moviescraper.py
@@ -9,6 +9,11 @@ import traceback
 import operator
 from lxml import etree
 
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
 import fanarttvapi
 import tmdbapi as tmdb
 from Movie import Movie
--- core/tvscraper.py
+++ core/tvscraper.py
@@ -3,6 +3,11 @@ import os
 import sys
 import time
 
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
 from tvdbapi import get_show_nfo
 from core.helpers import regex
 from core.helpers.config import config
