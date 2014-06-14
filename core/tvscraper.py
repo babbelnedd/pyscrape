b6643028b23e264d60e42375e2694eef3b129927
Lucas Schad <schad.lucas@gmail.com>
2014-06-14 20:25:14 +0200

    Fixed path for windows systems

--- core/tvscraper.py
+++ core/tvscraper.py
@@ -4,7 +4,7 @@ import os
 import sys
 import time
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 if not path in sys.path:
     sys.path.insert(1, path)
 del path
